47febaf6aab26fcfc6ec7d96a245653dff9b5587
陈辉 <chenhui@rock-chips.com>
2012-05-02 10:38:26 +0800

    TARGET_BOARD_PLATFORM is change from rockchip to rk29xx or rk30xx

--- Android.mk
+++ Android.mk
@@ -13,8 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-ifeq ($(TARGET_BOARD_PLATFORM),rockchip)
-
 include $(all-subdir-makefiles)
 
-endif
