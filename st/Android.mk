4da8b30a9ac8a7753acf1c729ce7df2104617300
luowei <lw@rock-chips.com>
2013-11-14 17:53:43 +0800

    update gyroscope l3g4200d lib

--- st/Android.mk
+++ st/Android.mk
@@ -46,7 +46,8 @@ LOCAL_SHARED_LIBRARIES := liblog \
 	libcutils \
 	libutils
 
-LOCAL_LDFLAGS = $(LOCAL_PATH)/LibFusion_ARM_cpp.a
+#LOCAL_LDFLAGS = $(LOCAL_PATH)/LibFusion_ARM_cpp.a
+LOCAL_LDFLAGS = $(LOCAL_PATH)/MEMSAlgLib_SI_ARM_cpp.a
 
 LOCAL_PRELINK_MODULE := false
 
